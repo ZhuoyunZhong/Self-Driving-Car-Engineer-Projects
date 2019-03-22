f3600500b9baf17db8bc7ef8c36cf4244691a567
joeyzhong90595 <905954450@qq.com>
2019-03-23 03:50:25 +0800

    project9

---
+++ P9-PID-Control-Project/src/PID.h
@@ -0,0 +1,56 @@
+#ifndef PID_H
+#define PID_H
+
+class PID {
+ public:
+  /**
+   * Constructor
+   */
+  PID();
+
+  /**
+   * Destructor.
+   */
+  virtual ~PID();
+
+  /**
+   * Initialize PID.
+   * @param (Kp_, Ki_, Kd_) The initial PID coefficients
+   */
+  void Init(double Kp_, double Ki_, double Kd_);
+
+  /**
+   * Update the PID error variables given cross track error.
+   * @param cte The current cross track error
+   */
+  void UpdateError(double cte);
+
+  /**
+   * Calculate the total PID error.
+   * @output The total PID error
+   */
+  double TotalError();
+
+ private:
+  /**
+   * PID Errors
+   */
+  double p_error;
+  double i_error;
+  double d_error;
+
+  /**
+   * PID Coefficients
+   */ 
+  double Kp;
+  double Ki;
+  double Kd;
+  
+  /**
+   * Helper Variables
+   */
+  double sum_cte;
+  double previous_cte;
+};
+
+#endif  // PID_H
